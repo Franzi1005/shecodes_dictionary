89c6fb30bbb4e8ed3952bc2089941cc23f2cff31
Franzi1005 <fschallhorn@gmail.com>
2023-04-18 18:22:29 +0200

    Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <button type="button" className="btn btn-primary">
+        {" "}
+        hello
+      </button>
+      hier ist jetzt text was klappt hier nicht?
     </div>
   );
 }
-
-export default App;
